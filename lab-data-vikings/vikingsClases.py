8f7315e46d3b0a2f2170484d1e971cf0cc6ec9c2
rshernandez10 <62353650+rshernandez10@users.noreply.github.com>
2020-04-12 21:01:29 -0500

    Done

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,24 +1,66 @@
 
 # Soldier
 
-
 class Soldier:
     pass
+    def __init__(health, strength):
+    self.health = health
+    self.strength = strength
+    
+def attack()
+    return self.strength;
 
+def receiveDamage(damage);
+    self.health -= damage;
+  
 # Viking
 
-
-class Viking:
+class Viking(Soldier):
     pass
+    def __init__(self,name, health, strength):
+    super().__init__(health, strength)
+    self.name = name
+
+attack()
+    return self.strenght;
+
+def receiveDamage(damage);
+    self.health -= damage;
+    if char.health > 0:
+        self.name +' has received ' + str(damage) + 'points of damage'
+    else:
+        self.name +' has died in act of combat.'
+    
+battleCry(self)
+    return 'Odin owns you all!'
 
 # Saxon
 
-
-class Saxon:
+class Saxon(Soldier):
     pass
+    def __init__(self, health, strength)
+    super().__init__(health, strength)
+    
+    attack
+        return self.strength;
+    
+def receiveDamage(damage);
+    self.health -= damage;
+    if char.health > 0:
+        'A saxon has received ' + str(damage) + 'points of damage'
+    else:
+        'A saxon has died in act of combat.'
 
 # War
 
-
 class War:
-    pass
+    def _init__(self)
+    self.vikingArmy = []
+    self.saxonArmy = []
+    
+    def addViking(self, Viking):
+        self.VikingArmy.append(Viking)
+        
+    def addSaxon(self, Saxon):
+        self.SaxonArmy.append(Saxon)
+    
