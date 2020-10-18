91f7023a64fc79a068d1eb384e64fff40cb2f4a3
PaoMg <pao.mg099@gmail.com>
2020-10-18 18:20:06 -0500

    Lab data vikings

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,24 +1,76 @@
 
 # Soldier
-
+import random
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self,damage):
+        #should remove the received damage from the `health` property
+        self.health = self.health - damage
+        return
 
 # Viking
 
+#Viking should inherit from `Soldier`
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
 
-class Viking:
-    pass
+#attack method should be **inherited** from `Soldier`, no need to reimplement it
+    def receiveDamage(self,damage):
+        #should remove the received damage from the `health` property
+        self.health = self.health - damage
+        if self.health > 0:
+            return self.name + "has received " + str(damage) + "points of damage"
+        else:
+            return self.name + "has died in act of combat"
+            
+    def battleCry(self):
+         return 'Odin Owns You All!'
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+    
+    def receiveDamage(self,damage):
+        #should remove the received damage from the `health` property
+        self.health = self.health - damage
+        if self.health > 0:
+            return "A Saxon has received " + str(damage) + "points of damage"
+        else:
+            return "A Saxon has died in combat"
 
 # War
 
 
 class War:
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+
+    #def vikingAttack():
+       
+    
+    #def saxonAttack()
+
+    #def showStatus()
     pass
