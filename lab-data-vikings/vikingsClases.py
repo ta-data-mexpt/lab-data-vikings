5f166e506d2ea143bb97ced9f3fba8c307077068
Cesar Bravo <cesar.bo88@gmail.com>
2020-10-17 21:24:09 -0500

    first commit of lab Object Oriented Programming

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,16 +1,65 @@
 
 # Soldier
 
-
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        return None
+# ------------------------- SOLDIER TEST -------------------------------
+
+#test_sold1= Soldier(100,30)
+#test_sold2 = Soldier(100,50)
+
+#print(test_sold2.health)
+
+#test_sold2.receiveDamage(test_sold1.attack())
+
+#print(test_sold1.strength)
+#print(test_sold2.health)
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
 
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > 0:
+            print(self.name, 'has received', damage, 'points of damage')
+        else:
+            print(self.name, 'has died in act of combat')
+        return
+
+    def battleCry(self):
+        return print('Odin Owns You All!')
+
+# -------------------------- Viking Test ------------------------------------
+
+test_vik1= Viking('Ragnar',120,40)
+test_vik2 = Viking('Rollo',80,60)
+
+print(test_vik2.health)
+
+test_vik2.receiveDamage(test_vik1.attack())
+
+print(test_vik1.strength)
+print(test_vik2.health)
+
+test_vik2.receiveDamage(test_vik1.attack())
+
+test_vik2.battleCry()
 # Saxon
 
 
