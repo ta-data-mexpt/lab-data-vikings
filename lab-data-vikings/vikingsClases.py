5751b5e305c34a77dbf1f2036025dbbc92aa5beb
Miguel Diaz <miguel.d.ferrusca@gmail.com>
2021-04-24 16:05:10 -0500

    1st try before tests

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -3,18 +3,61 @@
 
 
 class Soldier:
+    def __init__(self, health, strength):
+        
+        self.health = health
+        self.strength = strenght
+        
+        def attack():
+            return self.strength
+        
+        def receiveDamage(damage):
+            self.health = self.health - damage
     pass
 
 # Viking
 
 
-class Viking:
+class Viking(Soldier):
+    
+    def __init__(self, name, health, strength):
+        
+        super().__inti__(self, health, strength)
+        self.name = name
+        
+        def receiveDamage(damage):
+            self.health = self.health - damage
+            if self.health <= 0:
+                message = name + " has died in act of combat"
+                return message
+            else:
+                message = name + " has received "+ damage + "points of damage"
+                return message
+            
+        def battleCry():
+            message = "Odin Owns You All!"
+            return message
+            
     pass
 
 # Saxon
 
 
 class Saxon:
+    
+    def __init__(self, health, strength):
+        
+        super().__inti__(self, health, strength)
+        
+        def receiveDamage(damage):
+            self.health = self.health - damage
+            if self.health <= 0:
+                message = "A Saxon has died in combat"
+                return message
+            else:
+                message = "A Saxon has received "+ damage + "points of damage"
+                return message
+    
     pass
 
 # War
