17851de29614e3d2a93af92ddc34153be1305dac
Miguel Diaz <miguel.d.ferrusca@gmail.com>
2021-04-25 19:30:38 -0500

    solved

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -8,12 +8,11 @@ class Soldier:
         self.health = health
         self.strength = strength
         
-        def attack(self):
-            return strength
+    def attack(self):
+        return self.strength
         
-        def receiveDamage(self,damage):
-            self.health = self.health - damage
-    pass
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
 
 # Viking
 
@@ -21,44 +20,40 @@ class Soldier:
 class Viking(Soldier):
     
     def __init__(self, name, health, strength):
-        
-        super().__inti__(self, health, strength)
+        Soldier.__init__(self, health, strength)
         self.name = name
         
-        def receiveDamage(damage):
-            self.health = self.health - damage
-            if self.health <= 0:
-                message = name + " has died in act of combat"
-                return message
-            else:
-                message = name + " has received "+ damage + "points of damage"
-                return message
-            
-        def battleCry():
-            message = "Odin Owns You All!"
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health <= 0:
+            message = self.name + " has died in act of combat"
+            return message
+        else:
+            message = self.name + " has received "+ str(damage) + " points of damage"
             return message
             
-    pass
+    def battleCry(self):
+        message = "Odin Owns You All!"
+        return message
+            
 
 # Saxon
 
 
-class Saxon:
+class Saxon(Soldier):
     
-    def __init__(self, health, strength):
+#    def __init__(self, health, strength):
+#        super().__init__(self, health, strength)
         
-        super().__inti__(self, health, strength)
-        
-        def receiveDamage(damage):
-            self.health = self.health - damage
-            if self.health <= 0:
-                message = "A Saxon has died in combat"
-                return message
-            else:
-                message = "A Saxon has received "+ damage + "points of damage"
-                return message
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health <= 0:
+            message = "A Saxon has died in combat"
+            return message
+        else:
+            message = "A Saxon has received "+ str(damage) + " points of damage"
+            return message
     
-    pass
 
 # War
 
